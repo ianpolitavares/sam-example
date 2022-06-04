0e8880434bfc699b85a8b6006a660f93a4f6e332
IanPoli <ianpolit@gmail.com>
2022-06-04 15:56:27 -0300

    first commit

--- app/src/lambda_handler.py
+++ app/src/lambda_handler.py
@@ -1,8 +1,20 @@
+import boto3
+import json
+
+
 def hello(event, context):
-    x = 10
-    y = 10
+    
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket('sam-app-srcbucket-16f8dlu093ts5')
 
-    z = x * y
-    print(z)
+    for my_bucket_object in bucket.objects.all():
+        if my_bucket_object.key == 'tbrzien.json' or my_bucket_object.key == 'tbrzien2.json':
+            print('entrou')
 
-    return z
+            body = my_bucket_object.get()['Body'].read()
+            loaded = body.decode('utf-8')
+            lines = loaded.splitlines()
+            print(lines)
+
+
+    return "end"
